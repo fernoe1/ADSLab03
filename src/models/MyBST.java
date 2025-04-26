9d1a1348736dc64a1d6b335dc435db5917eef21d
fernoe1 <krutoytemirlan2007@mail.ru>
2025-04-26 22:14:49 +0600

    documentation

--- src/models/MyBST.java
+++ src/models/MyBST.java
@@ -4,6 +4,11 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Stack;
 
+/**
+ * Binary Search Tree implementation.
+ * @param <K> the key
+ * @param <V> the value
+ */
 public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.MyNode<K, V>> {
     public class MyNode<K, V> {
         private K key;
@@ -30,7 +35,14 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
         size = 0;
     }
 
+    /**
+     * Puts a new key or updates existing keys value. <br>
+     * O(log n) time complexity.
+     * @param key the key
+     * @param val the value
+     */
     public void put(K key, V val) {
+        // if the root is null, we initialize it
         if (root == null) {
             root = new MyNode<>(key, val);
 
@@ -40,12 +52,15 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
 
         MyNode<K, V> current = root;
         while (current != null) {
+            // if a node with that key already exists, update its value
             if (key.compareTo(current.key) == 0) {
                 current.val = val;
 
                 return;
             }
 
+            // traverse right if the key is bigger than the current key
+            // and initialize a new node if it does not exist
             if (key.compareTo(current.key) > 0) {
                 if (current.right == null) {
                     current.right = new MyNode<>(key, val);
@@ -57,6 +72,7 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
                 current = current.right;
             }
 
+            // same thing as above but for left
             if (key.compareTo(current.key) < 0) {
                 if (current.left == null) {
                     current.left = new MyNode<>(key, val);
@@ -70,6 +86,11 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
         }
     }
 
+    /**
+     * Gets specific key's value from BST.
+     * @param key the key
+     * @return the value of that key
+     */
     public V get(K key) {
        try {
            return (V) findNode(root, key).val;
@@ -78,29 +99,52 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
        }
     }
 
+    /**
+     * Returns size of the BST.
+     * @return the size
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Helper method that finds node using a key. <br>
+     * O(log n) time complexity.
+     * @param root the root of the BST
+     * @param key the key to search for
+     * @return the node with that key
+     * @throws NullPointerException if the key does not exist
+     */
     private MyNode<K, V> findNode(MyNode<K, V> root, K key) {
         while (root != null) {
+            // if the key is found return the node
             if (key.compareTo(root.key) == 0) {
 
                 return root;
             }
 
+            // traverse right
             if (key.compareTo(root.key) > 0 ) {
                 root = root.right;
             }
 
+            // traverse left
             if (key.compareTo(root.key) < 0) {
                 root = root.left;
             }
         }
 
+        // throw null pointer exception if a node with specified key
+        // does not exist
         throw new NullPointerException("A " + key + " key does not exist");
     }
 
+    /**
+     * Deletes a node with specified key from the BST. <br>
+     * O(log n) time complexity.
+     * @param key the key of the node to delete
+     * @throws NullPointerException if a node with the specified key does not exist
+     */
     public void delete(K key) {
         MyNode<K, V> current = root;
         MyNode<K, V> parent = null;
@@ -108,13 +152,16 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
         while (current != null) {
             if (key.compareTo(current.key) == 0) {
 
-                // leaf
+                // deleting a leaf
                 if (current.left == null && current.right == null) {
                     if (parent == null) {
+                        // if it's the root, make it null
                         root = null;
                     } else if (parent.left == current) {
+                        // if it's the left leaf, make the left pointer null
                         parent.left = null;
                     } else {
+                        // if it's the right, make the right pointer null
                         parent.right = null;
                     }
 
@@ -122,16 +169,20 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
                     return;
                 }
 
-                // two children
+                // deleting a node with two children
                 if (current.left != null && current.right != null) {
+                    // finding inOrder and inOrderParent
                     MyNode<K, V> inOrder = findInOrder(current);
                     MyNode<K, V> inOrderParent = findInOrderParent(current);
 
+                    // copying the value and key of the inOrder
                     current.val = inOrder.val;
                     current.key = inOrder.key;
                     if (inOrder.right != null) {
+                        // making sure we don't lose children of the inOrder node
                         inOrderParent.left = inOrder.right;
                     } else if (inOrderParent.left == inOrder) {
+                        // if it does not have any children, make the pointers null
                         inOrderParent.left = null;
                     } else {
                         inOrderParent.right = null;
@@ -141,14 +192,16 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
                     return;
                 }
 
-                // one child
+                // deleting a node with one children
                 if (parent == null) {
+                    // if the root contains one child, make the next child the root
                     root = (current.left != null) ? current.left : current.right;
 
                     size--;
                     return;
                 } else {
                     if (parent.left == current) {
+                        // same thing but for a node
                         parent.left = (current.left != null) ? current.left : current.right;
                     } else {
                         parent.right = (current.left != null) ? current.left : current.right;
@@ -159,6 +212,7 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
                 }
             }
 
+            // traversing
             parent = current;
             if (key.compareTo(current.key) < 0) {
                 current = current.left;
@@ -167,14 +221,22 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
             }
         }
 
+        // throw null pointer exception if a node with the given key does not exist
         if (current == null) {
             throw new NullPointerException("Key " + key + " does not exist.");
         }
     }
 
+    /**
+     * Finds in order node's parent. <br>
+     * O(log n) time complexity.
+     * @param start the node to start from
+     * @return the in order node's parent
+     */
     private MyNode<K, V> findInOrderParent(MyNode<K, V> start) {
         MyNode<K, V> parent = start;
         start = start.right;
+        // traversing
         while (start.left != null) {
             parent = start;
             start = start.left;
@@ -183,8 +245,15 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
         return parent;
     }
 
+    /**
+     * Finds in order node. <br>
+     * O(log n) time complexity.
+     * @param start the node to start from
+     * @return the in order node
+     */
     private MyNode<K, V> findInOrder(MyNode<K, V> start) {
         start = start.right;
+        // traversing
         while (start.left != null) {
             start = start.left;
         }
@@ -192,11 +261,18 @@ public class MyBST<K extends Comparable<K>, V> implements Iterable<MyBST<K, V>.M
         return start;
     }
 
+    /**
+     * Return iterator for for-each.
+     * @return the in order iterator
+     */
     @Override
     public Iterator<MyNode<K, V>> iterator() {
         return new MyBSTIterator();
     }
 
+    /**
+     * Implementation of in order iterator using stack logical data structure.
+     */
     private class MyBSTIterator implements Iterator<MyNode<K, V>> {
         private Stack<MyNode<K, V>> stack;
 
--- src/models/MyHashTable.java
+++ src/models/MyHashTable.java
@@ -1,5 +1,10 @@
 package models;
 
+/**
+ * Implementation of Hash Table.
+ * @param <K> the key
+ * @param <V> the value
+ */
 public class MyHashTable<K, V> {
     private class HashNode<K, V> {
         private K key;
@@ -26,13 +31,24 @@ public class MyHashTable<K, V> {
         size = 0;
     }
 
+    /**
+     * Get length of chain array.
+     * @return the length of chain array
+     */
     public int getM() {
         return M;
     }
 
+    /**
+     * Get the size of a bucket. <br>
+     * O(n) time complexity.
+     * @param index the index of the bucket
+     * @return the size of that bucket
+     */
     public int getBucketSize(int index) {
         HashNode<K, V> current = chainArr[index];
         int size = 0;
+        // iterate through the bucket till the end
         while (current != null) {
             size++;
             current = current.next;
@@ -41,24 +57,41 @@ public class MyHashTable<K, V> {
         return size;
     }
 
+    /**
+     * Runs the hash code method on the key.
+     * @param key the key to hash
+     * @return hashed value
+     */
     private int hash(K key) {
         return Math.abs(key.hashCode());
     }
 
+    /**
+     * Hashes the key, calculates the index, and puts a new node at that specified bucket. <br>
+     * If a node with that key already exists, updates its value. <br>
+     * O(1) best O(n) worst time complexity.
+     * @param key the key
+     * @param value the value
+     */
     public void put(K key, V value) {
         int index = hash(key) % M;
         if (chainArr[index] == null) {
+            // if the root at the given index does not exist
+            // initialize it
             chainArr[index] = new HashNode<>(key, value);
             size++;
         } else {
+            // if the node exists, iterate till the end
             HashNode<K, V> root = chainArr[index];
             while (root != null) {
                 if (root.key.equals(key)) {
+                    // update the value if a node with that key already exists
                     root.val = value;
 
                     return;
                 }
                 if (root.next == null) {
+                    // create new node
                     root.next = new HashNode<>(key, value);
 
                     size++;
@@ -69,6 +102,12 @@ public class MyHashTable<K, V> {
         }
     }
 
+    /**
+     * Gets the value of specified key. <br>
+     * O(1) best O(n) worst time complexity.
+     * @param key the key
+     * @return the value of that specified key
+     */
     public V get(K key) {
         try {
             return findNode(chainArr[hash(key) % M], key).val;
@@ -77,7 +116,15 @@ public class MyHashTable<K, V> {
         }
     }
 
+    /**
+     * Finds the node with a specified key.
+     * @param root the root of the bucket
+     * @param key the specified key
+     * @return the node with that specified key
+     * @throws NullPointerException if the node with that specified key does not exist
+     */
     private HashNode<K, V> findNode(HashNode<K, V> root, K key) throws NullPointerException {
+        // iterate through root till the node
         while (root != null) {
             if (root.key.equals(key)) {
 
@@ -89,6 +136,12 @@ public class MyHashTable<K, V> {
         throw new NullPointerException("A " + key + " key does not exist");
     }
 
+    /**
+     * Removes the node with specified key from the hash table. <br>
+     * O(1) best O(n) worst time complexity.
+     * @param key the key
+     * @return the value of the removed node
+     */
     public V remove(K key) {
         V value = null;
         HashNode<K, V> current = chainArr[hash(key) % M];
@@ -97,10 +150,13 @@ public class MyHashTable<K, V> {
             if (current.key.equals(key)) {
                 value = current.val;
                 if (previous == null && current.next == null) {
+                    // make the root null
                     chainArr[hash(key) % M] = null;
                 } else if (previous == null && current.next != null) {
+                    // change the root to the next node
                     chainArr[hash(key) % M] = current.next;
                 } else {
+                    // cut pointers to the middle node
                     previous.next = current.next;
                 }
 
@@ -114,9 +170,16 @@ public class MyHashTable<K, V> {
         return null;
     }
 
+    /**
+     * Returns true or false depending on if the hash table contains specified value. <br>
+     * O(n) time complexity.
+     * @param value the value
+     * @return true or false
+     */
     public boolean contains(V value) {
         for (int i = 0; i < M; i++) {
             HashNode<K, V> current = chainArr[i];
+            // iterate through all buckets
             while (current != null) {
                 if (current.val.equals(value)) {
 
@@ -129,9 +192,16 @@ public class MyHashTable<K, V> {
         return false;
     }
 
+    /**
+     * Returns the key of the specified value. <br>
+     * O(n) time complexity.
+     * @param value the value
+     * @return the key
+     */
     public K getKey(V value) {
         for (int i = 0; i < M; i++) {
             HashNode<K, V> current = chainArr[i];
+            // iterate through all buckets
             while (current != null) {
                 if (current.val.equals(value)) {
 
